68afcee74ceb68caaf91631e0ce5d84d5b2f1078
Ahamd Daifullah <adefsing@gmail.com>
2020-11-02 16:20:29 +0700

    Dashboard & data user

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -10,6 +10,73 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $this->load->view('admin/v_home');
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_dashboard');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function user()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_user');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function santri()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_santri');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function pembayaran()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_pembayaran');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function perizinan()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_perizinan');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function pelanggaran()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_pelanggaran');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function kompetensi()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_kompetensi');
+        $this->load->view('admin_template/footer');
+    }
+
+    public function dataWebUser()
+    {
+
+        $this->load->view('admin_template/header');
+        $this->load->view('admin_template/mainmenu');
+        $this->load->view('admin/v_dataWebuser');
+        $this->load->view('admin_template/footer');
     }
 }
---
+++ application/views/admin_template/mainmenu.php
@@ -0,0 +1,32 @@
+   <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="<?php echo base_url(); ?>theme-assets/images/backgrounds/02.jpg">
+       <div class="navbar-header">
+           <ul class="nav navbar-nav flex-row">
+               <li class="nav-item mr-auto"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="Pprrsa admin logo" src="<?php echo base_url(); ?>assets/Login1/images/logo_pondok.png" />
+                       <h3 class="brand-text">PPRRSA Admin Page</h3>
+                   </a></li>
+               <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
+           </ul>
+       </div>
+       <div class="main-menu-content">
+           <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+               <li class="active"><a href="index.html"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
+               </li>
+               <li class=" nav-item"><a href="charts.html"><i class="ft-pie-chart"></i><span class="menu-title" data-i18n="">Data User</span></a>
+               </li>
+               <li class=" nav-item"><a href="icons.html"><i class="ft-droplet"></i><span class="menu-title" data-i18n="">Data Santri</span></a>
+               </li>
+               <li class=" nav-item"><a href="cards.html"><i class="ft-layers"></i><span class="menu-title" data-i18n="">Pembayaran</span></a>
+               </li>
+               <li class=" nav-item"><a href="buttons.html"><i class="ft-box"></i><span class="menu-title" data-i18n="">Perizinan</span></a>
+               </li>
+               <li class=" nav-item"><a href="typography.html"><i class="ft-bold"></i><span class="menu-title" data-i18n="">Pelanggaran</span></a>
+               </li>
+               <li class=" nav-item"><a href="tables.html"><i class="ft-credit-card"></i><span class="menu-title" data-i18n="">Kompetensi</span></a>
+               </li>
+               <li class=" nav-item"><a href="form-elements.html"><i class="ft-layout"></i><span class="menu-title" data-i18n="">Data Web User</span></a>
+               </li>
+
+           </ul>
+       </div>
+       <div class="navigation-background"></div>
+   </div>
