c880aab0d074ecd1f619c3c7f6322e3522b5a4b4
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-11-11 15:30:34 +0700

    update model perizinan : add fuction tambah data, tampil data akhir dan

--- application/models/m_perizinan.php
+++ application/models/m_perizinan.php
@@ -3,7 +3,7 @@
 class M_perizinan extends CI_Model{
 
 function tampil_data(){
-		 $query= $this->db->query("SELECT tb_perizinan.id_perizinan, tb_perizinan.alasan, tb_perizinan.tgl_izin, tb_perizinan.tgl_datang, tb_perizinan.NIS, tb_santri.nama_santri FROM tb_perizinan JOIN tb_santri ON tb_perizinan.NIS=tb_santri.NIS;");
+		 $query= $this->db->query("SELECT tb_perizinan.id_perizinan, tb_perizinan.alasan, tb_perizinan.tgl_izin, tb_perizinan.tgl_datang, tb_perizinan.NIS,tb_perizinan.keterangan, tb_santri.nama_santri FROM tb_perizinan JOIN tb_santri ON tb_perizinan.NIS=tb_santri.NIS;");
     return $query;
   
 	}
@@ -12,5 +12,21 @@ function hapus_data($where,$table){
 		$this->db->where($where); 
 		$this->db->delete($table);
 	}
+
+function tambah_data($data, $table)
+   {
+       $this->db->insert($table, $data);
+   }
+
+    function tampil_data_akhir()
+  {
+    $this->db->order_by('id_perizinan', 'DESC');
+    return $this->db->get('tb_perizinan', 1);
+  }
+
+function tampil_perizinan()
+   {
+      return $this->db->get('tb_perizinan');
+   }
 
 }
