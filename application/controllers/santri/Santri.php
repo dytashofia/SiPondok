ffe4ed70c5e192aac8fd60a22e02e0167c132327
Ahamd Daifullah <adefsing@gmail.com>
2020-11-30 02:33:25 +0700

    dashboar santri 1%

---
+++ application/controllers/santri/Santri.php
@@ -0,0 +1,20 @@
+    
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Santri extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+
+        $this->load->view('santri_template/header');
+        //$this->load->view('santri_template/mainmenu');
+        $this->load->view('santri/v_dashboard');
+        $this->load->view('santri_template/footer');
+    }
+}
