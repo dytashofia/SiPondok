a771c26908d1277ad0a68b616e18c47789b894b3
Ahamd Daifullah <adefsing@gmail.com>
2020-12-03 08:37:24 +0700

    info pembayaran

--- application/controllers/santri/Santri.php
+++ application/controllers/santri/Santri.php
@@ -36,6 +36,14 @@ class Santri extends CI_Controller
         $this->load->view('santri_template/footer');
     }
 
+    public function infopembayaran()
+    {
+        $this->load->view('santri_template/header');
+        $this->load->view('santri/v_infopembayaran');
+        $this->load->view('santri_template/profile');
+        $this->load->view('santri_template/footer');
+    }
+
 
     public function pelanggaran()
     {
--- application/views/santri/v_dashboard.php
+++ application/views/santri/v_dashboard.php
@@ -162,7 +162,7 @@
                                          <p class="ab">KOMPETENSI</p>
                                      </button>
                                  </a>
-                                 <a href="<?php echo base_url(); ?>santri/Santri/pembayaran">
+                                 <a href="<?php echo base_url(); ?>santri/Santri/infopembayaran">
                                      <button type="button" class="btn bm btn-icon btn-secondary  mr-1 ml-1 mb-2 mt-2"><i class="ft-watch as ad"></i>
                                          <p class="ab">PEMBAYARAN</p>
                                          <span class="badge badge-pill badge-danger">5</span>
