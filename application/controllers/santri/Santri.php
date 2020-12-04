9156685c7c55c4b0a050e9a0fdc01f3e64040590
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-12-04 16:54:19 +0700

    tambah fuction aksi edit, hapus dan tambah perizinan pada controller santri

--- application/controllers/santri/Santri.php
+++ application/controllers/santri/Santri.php
@@ -8,7 +8,7 @@ class Santri extends CI_Controller
         parent::__construct();
         $this->simple_login2->cek_login();
         $this->load->model('m_user_pembayaran');
-
+        $this->load->model('m_perizinansantri');
         $this->load->helper('url','form');
     }
 
@@ -221,11 +221,162 @@ class Santri extends CI_Controller
     }
 
 
-    public function perizinan()
+
+//=================================================PERIZINAN====================================================//
+     public function perizinan()
     {
+        
+        $data = $this->m_perizinansantri->tampil_data()->num_rows();
+        if ($data > 0) {
+            // Mengambil id soal sebelumnya
+            $lastId = $this->m_perizinansantri->tampil_data_akhir()->result();
+            // Melakukan perulangan untuk mengambil data
+            foreach ($lastId as $row) {
+                // Melakukan pemisahan huruf dengan angka pada id perizinan
+                $rawid_perizinan = substr($row->id_perizinan, 3);
+                // Melakukan konversi nilai pemisahan huruf dengan angka pada id perizinn menjadi integer
+                $id_perizinanInt = intval($rawid_perizinan);
+
+                // Menghitung panjang id yang sudah menjadi integer
+                if (strlen($id_perizinanInt) == 1) {
+                    // jika panjang id hanya 1 angka
+                    $id_perizinan = "IZ00" . ($id_perizinanInt + 1);
+                } else if (strlen($id_perizinanInt) == 2) {
+                    // jika panjang id hanya 2 angka
+                    $id_perizinan = "IZ0" . ($id_perizinanInt + 1);
+                } else if (strlen($id_perizinanInt) == 3) {
+                    // jika panjang id hanya 3 angka
+                    $id_perizinan = "IZ" . ($id_perizinanInt + 1);
+                }
+            }
+        } else {
+            // Jika jumlah perizinan kurang dari sama dengan 0
+            $id_perizinan = "IZ001";
+        }
+
+        // Mengambil data mata pelajaran menggunakan model
+
+        $data = $this->m_perizinansantri->tampil_data()->result();
+        $izin = $this->m_perizinansantri->tampil_data()->result();
+
+        $data = array(
+            'id_perizinan' => $id_perizinan,
+            'izin' => $izin
+        );
+
         $this->load->view('santri_template/header');
-        $this->load->view('santri/v_perizinan');
+        $this->load->view('santri/v_perizinan', $data);
         $this->load->view('santri_template/profile');
         $this->load->view('santri_template/footer');
     }
+
+
+    public function aksiTambahperizinan()
+    {
+        $id_perizinan = $this->input->post('id_perizinan');
+        $NIS = $this->input->post('NIS');
+        $tgl_izin = $this->input->post('tgl_izin');
+        $tgl_datang = $this->input->post('tgl_datang');
+        $alasan = $this->input->post('alasan');
+        $keterangan = $_FILES['keterangan'];
+        $status = $this->input->post('status');
+
+        if ($keterangan = '') {
+        } else {
+            $config['upload_path']          = './assets/file_izin';
+            $config['allowed_types']        = 'jpg|png|jpeg|gif|JPG|JPEG|pdf';
+
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('keterangan')) {
+                $keterangan = $this->upload->data('file_name');
+            } else {
+                $keterangan = $this->upload->data('file_name');
+            }
+        }
+
+
+        $data = array(
+            'id_perizinan' => $id_perizinan,
+            'NIS' => $NIS,
+            'tgl_izin' => $tgl_izin,
+            'tgl_datang' => $tgl_datang,
+            'alasan' => $alasan,
+            'keterangan' => $keterangan,
+            'status' => $status
+        );
+
+        $this->m_perizinansantri->tambah_data($data, 'tb_perizinan');
+        redirect('index.php/santri/Santri/perizinan');
+    }
+
+    function hapusperizinan($id)
+    {
+        $where = array('id_perizinan' => $id);
+        $foto = $this->db->get_where('tb_perizinan', $where);
+        $this->m_perizinansantri->hapus_data($where, 'tb_perizinan');
+
+        if ($foto->num_rows($where) > 0) {
+            $pros = $foto->row();
+            $name = $pros->keterangan;
+
+            if (file_exists($lok = FCPATH . '/assets/file_izin/' . $name)) {
+                unlink($lok);
+            }
+        }
+
+        redirect('index.php/santri/Santri/perizinan');
+    }
+
+    public function updateperizinan()
+    {
+        $id_perizinan = $this->input->post('id_perizinan');
+        $NIS = $this->input->post('NIS');
+        $tgl_izin = $this->input->post('tgl_izin');
+        $tgl_datang = $this->input->post('tgl_datang');
+        $alasan = $this->input->post('alasan');
+        $status = $this->input->post('status');
+        $keterangan = $_FILES['keterangan'];
+        $where = array('id_perizinan' => $id_perizinan);
+        $foto = $this->db->get_where('tb_perizinan', $where);
+
+        if ($keterangan = '') {
+        } else {
+            $config['upload_path']          = './assets/file_izin';
+            $config['allowed_types']        = 'jpg|png|jpeg|gif|JPG|JPEG|pdf';
+
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('keterangan')) {
+            } else {
+                if ($foto->num_rows() > 0) {
+                    $pros = $foto->row();
+                    $name = $pros->keterangan;
+
+                    if (file_exists($lok = FCPATH . '/assets/file_izin/' . $name)) {
+                        unlink($lok);
+                    }
+                }
+
+                $keterangan = $this->upload->data('file_name');
+            }
+        }
+
+        $data = array(
+            'id_perizinan' => $id_perizinan,
+            'NIS' => $NIS,
+            'tgl_izin' => $tgl_izin,
+            'tgl_datang' => $tgl_datang,
+            'alasan' => $alasan,
+            'status'   => $status
+        );
+
+        if ($keterangan != NULL) {
+
+            $data['keterangan'] = $keterangan;
+        }
+
+        $this->m_perizinansantri->update_data($where, $data);
+        redirect('index.php/admin/Admin/perizinan');
+    }
+
+    //===============================================EDNPERIZINAN=================================================//
 }
